319a9171730e8e12f9c335948809d09f08f06c10
Tamani Phiri <tamanigabriel0@gmail.com>
2024-05-30 10:58:35 +0200

    init

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "react-hot-toast";
 
 const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
+      <Toaster position="top-center" />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
