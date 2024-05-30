33aea6ee213d7d597f4dd0fc97d9a48a24b10e7c
Tamani Phiri <tamanigabriel0@gmail.com>
2024-05-30 10:22:23 +0200

    init

---
+++ src/api/helpers/helpers.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const fetchImages = async (page: number, limit: number) => {
+  const res = await axios.get(
+    `https://picsum.photos/v2/list?page=${page}&limit=${limit}`
+  );
+  return res.data;
+};
---
+++ src/hooks/useImages.tsx
@@ -0,0 +1,19 @@
+import { useInfiniteQuery } from "react-query";
+import { fetchImages } from "../api/helpers/helpers";
+
+const useImages = (limit: number) => {
+  return useInfiniteQuery(
+    ["images"],
+    ({ pageParam = 1 }) => fetchImages(pageParam, limit),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (lastPage.length < limit) {
+          return undefined;
+        }
+        return pages.length + 1;
+      },
+    }
+  );
+};
+
+export default useImages;
