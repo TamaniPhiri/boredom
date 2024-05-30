8f956054d3ed690defe0a547efa527eb7af3ba0e
Tamani Phiri <tamanigabriel0@gmail.com>
2024-05-30 10:26:57 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useEffect } from "react";
+import "./App.css";
+import useImages from "./hooks/useImages";
+import { useInView } from "react-intersection-observer";
+import { IImage } from "./types/interface";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useImages(30);
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage, hasNextPage]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="gallery">
+      {data?.pages.map((page, pageIndex) => (
+        <Fragment key={pageIndex}>
+          {page.map((image: IImage) => (
+            <div key={image.id} className="image-item">
+              <img src={image.download_url} alt={image.author} />
+              <p>{image.author}</p>
+            </div>
+          ))}
+        </Fragment>
+      ))}
+      <div ref={ref} style={{ height: "20px" }} />
+      {isFetchingNextPage && <p>Loading more images...</p>}
+    </div>
+  );
 }
 
-export default App
+export default App;
