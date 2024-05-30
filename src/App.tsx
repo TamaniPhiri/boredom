1161c32fa5cb6dc46d86266d89bd6f417fd442c5
Tamani Phiri <tamanigabriel0@gmail.com>
2024-05-30 10:58:08 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import useImages from "./hooks/useImages";
 import { useInView } from "react-intersection-observer";
@@ -36,24 +36,58 @@ function App() {
   };
 
   return (
-    <div className=" bg-slate-500">
+    <section className=" min-h-screen w-full flex flex-col items-center justify-center px-4 md:px-12">
       {data?.pages.map((page, pageIndex) => (
-        <Fragment key={pageIndex}>
-          {page.map((image: IImage) => (
-            <div key={image.id}>
+        <div key={pageIndex} className="grid md:grid-cols-3 grid-cols-2 gap-4">
+          {page.map((item: IImage) => (
+            <div key={item.id} className="relative">
               <img
-                src={image.download_url}
-                alt={image.author}
-                className=" w-5 h-7"
+                src={item.download_url}
+                alt={item.author}
+                className="w-full h-full rounded-2xl object-center object-cover"
+                loading="lazy"
               />
-              <p>{image.author}</p>
+              <div className="flex gap-4 items-center">
+                <button onClick={() => handleCopy(item.url)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+                    />
+                  </svg>
+                </button>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           ))}
-        </Fragment>
+        </div>
       ))}
       <div ref={ref} style={{ height: "20px" }} />
       {isFetchingNextPage && <p>Loading more images...</p>}
-    </div>
+    </section>
   );
 }
 
